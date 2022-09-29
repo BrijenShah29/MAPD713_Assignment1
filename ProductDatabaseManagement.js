427cc7ea9e696cb285d0a80f70be8175e829532e
Brijen <bshah91@my.centennialcollege.ca>
2022-09-29 10:49:03 -0400

    implementing of productDatabaseManagement

---
+++ ProductDatabaseManagement.js
@@ -0,0 +1,15 @@
+var seneca = require('seneca')().use('productdatabase')
+
+var product = {
+    product:"laptop",
+    price: 201.99,
+    category: "PC"
+}
+
+function add_product() {
+    seneca.act({role: 'product',cmd:'add', data: product}, function(err,msg){
+        console.log(msg);
+    });
+}
+
+add_product();
